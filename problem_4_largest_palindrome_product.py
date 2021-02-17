f9784281f36f830f4ff8e78e10ca22806825d5c1
Евгений Тимофеев <eygeniy-t@office.taxnet.ru>
2021-02-17 16:54:42 +0300

    hz

--- problem_4_largest_palindrome_product.py
+++ problem_4_largest_palindrome_product.py
@@ -7,4 +7,21 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 
 """
+from utilities import benchmark
 
+# def benchmark(func):
+#   import time
+
+#   def wrapper():
+#     start = time.time()
+#     func()
+#     end = time.time()
+#     print('Время выполнения: {} секунд.'.format(end-start))
+#   return wrapper
+
+
+@benchmark
+def test():
+  print('OWO')
+
+test()
